9590e4cd5e5d38a3d698d50514508749e8c34efb
Nathan Griffiths <ngriffiths@postmedia.com>
2025-05-29 16:53:25 -0700

    initial commit

--- src/data/params.js
+++ src/data/params.js
@@ -1,8 +1,8 @@
 const params = {
     appId: 'app',
-    agencyId: 'dp-9', // find in the data page of your cloudtables dataset
-    clientId: 'pssdb-v11', // unique for each dataset
-    cloudTableId: 'fd3ab5e8-3064-11ed-a814-6bfc76c2745a', // find in embed tab
+    agencyId: 'dp-57', // find in the data page of your cloudtables dataset
+    clientId: 'pssdb-v12', // unique for each dataset
+    cloudTableId: '2e623496-3cb7-11f0-a98a-738f477aee66', // find in embed tab
     // below here probably won’t change 
     tableId: 'cloudtable', // DOM element for the table
     cloudTableDomain: 'vs-postmedia-a.cloudtables.me',
